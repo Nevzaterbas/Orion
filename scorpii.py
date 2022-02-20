41cdca9333d9f38fe834c613a60279bb9e01fd3b
dmrcghn <95675666+dmrcghn@users.noreply.github.com>
2022-02-20 22:29:17 +0000

    KCF tracking piece of concept

---
+++ scorpii.py
@@ -0,0 +1,83 @@
+from PyQt5 import QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+import cv2
+import numpy as np
+
+tracker = cv2.TrackerKCF_create()
+cap = cv2.VideoCapture(0)
+ret, cv_img = cap.read()
+bbox = cv2.selectROI(cv_img)
+ret = tracker.init(cv_img,bbox)
+
+
+class VideoThread(QThread):
+    change_pixmap_signal = pyqtSignal(np.ndarray)
+
+    def __init__(self):
+        super().__init__()
+        self._run_flag = True
+
+    def run(self):
+        while self._run_flag:
+            ret, cv_img = cap.read()
+            if not ret: continue
+            ret, bbox = tracker.update(cv_img)
+            if ret:
+                (x,y,w,h) = [int(v) for v in bbox]
+                cv2.rectangle(cv_img, (x,y), (x+w, y+h), (0, 255, 0), 2, 1)
+            else:
+                cv2.putText(cv_img, 'HATA', (100, 0), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            self.change_pixmap_signal.emit(cv_img)
+        cap.release()
+
+    def stop(self):
+        self._run_flag = False
+        self.wait()
+
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Scorpii")
+        self.resize(960, 540)
+        self.disply_width = 640
+        self.display_height = 480
+        self.image_label = QLabel(self)
+        self.image_label.resize(self.disply_width, self.display_height)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.image_label)
+
+        self.setLayout(vbox)
+
+        self.thread = VideoThread()
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        self.thread.start()
+
+    def closeEvent(self, event):
+        self.thread.stop()
+        event.accept()
+
+
+
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+        qt_img = self.convert_cv_qt(cv_img)
+        self.image_label.setPixmap(qt_img)
+
+    def convert_cv_qt(self, cv_img):
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    a = App()
+    a.show()
+    sys.exit(app.exec_())
