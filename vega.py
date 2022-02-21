0681c2cd551254eddc10846cfa813484ba82dc1b
dmrcghn <95675666+dmrcghn@users.noreply.github.com>
2022-02-21 05:19:32 +0000

    Seperate GUI project and YOLO PoC for now

--- vega.py
+++ vega.py
@@ -6,6 +6,13 @@ import sys
 import cv2
 import numpy as np
 
+net = cv2.dnn.readNet("yolo/yolov4-tiny.weights","yolo/yolov4-tiny.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV); net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL) # intel
+classes = []
+
+with open("yolo/coco.names","r") as f:
+    classes = f.read().splitlines()
+
 class VideoThread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray)
 
@@ -29,22 +36,16 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Deneb")
-        self.resize(640, 600)
+        self.resize(960, 540)
         self.disply_width = 640
         self.display_height = 480
         self.image_label = QLabel(self)
         self.image_label.resize(self.disply_width, self.display_height)
         self.textLabel = QLabel('RTSP stream')
-        self.radio1 = QRadioButton("Otonom Kalkis")
-        self.radio1.toggled.connect(self.kalkis)
-        self.radio2 = QRadioButton("Otonom Inis")
-        self.radio2.toggled.connect(self.inis)
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.image_label)
         vbox.addWidget(self.textLabel)
-        vbox.addWidget(self.radio1)
-        vbox.addWidget(self.radio2)
 
         self.setLayout(vbox)
 
@@ -56,18 +57,46 @@ class App(QWidget):
         self.thread.stop()
         event.accept()
 
-    def kalkis(self):
-        radioBtn = self.sender()
-        if radioBtn.isChecked():
-            QMessageBox.about(self, "Title", "Message")
 
-    def inis(self):
-        radioBtn = self.sender()
-        if radioBtn.isChecked():
-            QMessageBox.about(self, "Title", "Message")
 
     @pyqtSlot(np.ndarray)
     def update_image(self, cv_img):
+        height, width, _ = cv_img.shape
+        blob = cv2.dnn.blobFromImage(cv2.UMat(cv_img), 1/255, (416,416),(0,0,0), swapRB=True, crop=False)
+        net.setInput(blob)
+        output_layers_names = net.getUnconnectedOutLayersNames()
+        layerOutputs = net.forward(output_layers_names)
+        boxes = []
+        confidences = []
+        class_ids = []
+
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+
+                if confidence > 0.5:
+                    center_x = int(detection[0]*width)
+                    center_y = int(detection[1]*height)
+                    w = int(detection[2]*width)
+                    h = int(detection[3]*height)
+                    x = int(center_x - w/2)
+                    y = int(center_y - h/2)
+                    boxes.append([x, y, w, h])
+                    confidences.append((float(confidence)))
+                    class_ids.append(class_id)
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        font = cv2.FONT_HERSHEY_PLAIN
+        colors = np.random.uniform(0, 255, size=(len(boxes),3))
+        if len(indexes) > 0:
+            for i in indexes.flatten():
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence = str(round(confidences[i],2))
+                color = colors[i]
+                cv2.rectangle(cv_img, (x,y), (x+w , y+h), color,2)
+                cv2.putText(cv_img, label +"" + confidence, (x,y+20), font, 2, (255,255,255),2)
         qt_img = self.convert_cv_qt(cv_img)
         self.image_label.setPixmap(qt_img)
 
